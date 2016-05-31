20da399dc3c8ccc1df5647f67c05659430c8a239
Gogs <gogs@fake.local>
2016-05-31 14:39:40 +0800

    clean code.

--- client_proxy/main.cpp
+++ client_proxy/main.cpp
@@ -16,35 +16,6 @@ struct person
 	META(age, name);
 };
 
-void test_client()
-{
-	try
-	{
-		boost::asio::io_service io_service;
-		DeSerializer dr;
-		client_proxy client(io_service);
-		client.connect("192.168.2.154", "9000");
-		person p = { 20, "aa" };
-		//auto str = client.make_json("fun1", p, 1);
-		//client.call(str);
-		
-		std::string result = client.call("fun1", p, 1);
-		dr.Parse(result);
-
-		response_msg<int> response = {};
-		dr.Deserialize(response);
-		if (response.code == result_code::OK)
-		{
-			std::cout << response.result << std::endl;
-		}
-		io_service.run();
-	}
-	catch (const std::exception& e)
-	{
-		std::cerr << "Exception: " << e.what() << std::endl;
-	}
-}
-
 template<typename T>
 void handle_result(const char* result)
 {
@@ -109,36 +80,11 @@ void test_spawn_client()
 		{
 			client_proxy client(io_service);
 			client.async_connect("127.0.0.1", "9000", yield);
-			//auto str = client.make_json("fun1", p, 1);
-			//client.call(str);
 
 			std::string result = client.async_call("add", yield, 1,2);
 			handle_result<int>(result.c_str());
 		});
 		io_service.run();
-	}
-	catch (const std::exception& e)
-	{
-		std::cerr << "Exception: " << e.what() << std::endl;
-	}
-}
-
-void test_performance()
-{
-	try
-	{
-		boost::asio::io_service io_service;
-		
-		client_proxy client(io_service);
-		client.connect("192.168.2.154", "9000");
-
-		auto str = client.make_json("add", 1, 2);
-		std::thread thd([&io_service] {io_service.run(); });
-		std::this_thread::sleep_for(std::chrono::milliseconds(200));
-		while (true)
-		{
-			client.call(str);
-		}
 	}
 	catch (const std::exception& e)
 	{
@@ -156,124 +102,8 @@ void test_translate()
 		
 		std::string result = client.call("translate", "test");
 		handle_result<std::string>(result.c_str());
-		//std::this_thread::sleep_for(std::chrono::seconds(20));
+
 		io_service.run();
-	}
-	catch (const std::exception& e)
-	{
-		std::cerr << "Exception: " << e.what() << std::endl;
-	}
-}
-
-void test_upload()
-{
-	try
-	{
-		boost::asio::io_service io_service;
-		client_proxy client(io_service);
-		client.connect("127.0.0.1", "9000");
-		//client.connect("baidu.com", "12345", 5*1000);
-
-		std::ifstream file("client_proxy.sln", ios::binary);
-		if (!file.is_open())
-			return;
-
-		std::stringstream ss;
-		ss << file.rdbuf();
-		auto content = base64_encode(ss.str().c_str(), ss.str().length());
-		std::string result = client.call("upload", "test", content);
-		handle_result<bool>(result.c_str());
-		io_service.run();
-	}
-	catch (const std::exception& e)
-	{
-		std::cerr << "Exception: " << e.what() << std::endl;
-	}
-}
-
-void test_read()
-{
-	try
-	{
-		boost::asio::io_service io_service;
-		client_proxy client(io_service);
-		client.connect("127.0.0.1", "9000");
-		auto str = client.call("translate", "test");
-		std::cout << str << std::endl;
-		std::thread thd([&io_service] {io_service.run(); });
-
-		//wait for message from server.
-
-		bool ok = true;
-		while (ok)
-		{
-			size_t length = client.recieve();
-			
-			if (length != 0)
-				std::cout << client.recieve_data() << std::endl;
-		}
-
-		thd.join();
-//		io_service.run();
-	}
-	catch (const std::exception& e)
-	{
-		std::cerr << "Exception: " << e.what() << std::endl;
-	}
-}
-
-void test_sub()
-{
-	try
-	{
-		boost::asio::io_service io_service;
-		client_proxy client(io_service);
-		client.connect("127.0.0.1", "9000");
-		auto json_str = client.sub("translate");
-		std::cout << json_str << std::endl;
-		std::thread thd([&io_service] {io_service.run(); });
-
-		//wait for message from server.
-		bool ok = true;
-		while (ok)
-		{
-			size_t length = client.recieve();
-			
-			ok = length > 0;
-			if (ok)
-				std::cout << client.recieve_data() << std::endl;
-		}
-
-		thd.join();
-		std::string str;
-		std::cin >> str;
-		/*std::cout << str << std::endl;
-		io_service.run();*/
-	}
-	catch (const std::exception& e)
-	{
-		std::cerr << "Exception: " << e.what() << std::endl;
-	}
-}
-
-void test_pub()
-{
-	try
-	{
-		boost::asio::io_service io_service;
-		client_proxy client(io_service);
-		client.connect("127.0.0.1", "9000");
-		std::thread thd([&io_service] {io_service.run(); });
-		while (true)
-		{
-			client.pub("translate", "test");
-			std::string str;
-			std::cin >> str;
-			if (str != "pub")
-				break;
-		}
-
-		//io_service.run();
 	}
 	catch (const std::exception& e)
 	{
@@ -284,25 +114,7 @@ void test_pub()
 int main()
 {
 	log::get().init("rest_rpc_client.lg");
-	//test_performance();
-	//test_client();
-	//test_upload();
-	//while (true)
-	//{
-	//	std::string str;
-	//	std::cin >> str;
-	//	if (str == "pub")
-	//		test_pub();
-	//	else if (str == "sub")
-	//		test_sub();
-	//	else
-	//		break;
-	//}
-	
-	//test_sub();
-	//test_read();
 	test_translate();
-	//test_async_client();
-	//test_spawn_client();
+
 	return 0;
 }
--- main.cpp
+++ main.cpp
@@ -11,63 +11,9 @@
 #include "client_proxy/client_proxy.hpp"
 #include "log.hpp"
 
-struct person
-{
-	int age;
-	std::string name;
-
-	META(age, name);
-};
-
 int add(int a, int b)
 {
 	return a + b;
-}
-void hello()
-{
-	std::cout << "hello" << std::endl;
-}
-
-void test_one(int d)
-{
-	std::cout << d << std::endl;
-}
-
-void foo(std::string b, int a)
-{
-	std::cout << b << std::endl;
-}
-
-void fun(const person& ps)
-{
-	std::cout << ps.name << std::endl;
-}
-
-int fun1(const person& ps, int a)
-{
-	std::cout << ps.name <<" "<<a<< std::endl;
-	return a;
-}
-
-TEST_CASE(asio_test_server, false)
-{
-	server s(9000, std::thread::hardware_concurrency());
-	s.register_handler("fun1", &fun1);
-	s.register_handler("fun", &fun);
-	s.register_handler("add", &add);
-	s.register_handler("about", &hello);
-	s.register_handler("foo", &foo);
-	s.register_handler("test_one", &test_one);
-
-	s.run();
-	getchar();
-}
-
-TEST_CASE(asio_test, false)
-{
-	server s(9000, std::thread::hardware_concurrency());
-	s.run();
-	getchar();
 }
 
 struct messager
@@ -78,18 +24,6 @@ struct messager
 		for (auto & c : temp) c = toupper(c);
 		return temp;
 	}
-
-	bool upload(const std::string& filename, const std::string& content)
-	{
-		std::ofstream file(filename, ios::binary);
-		if (!file.is_open())
-			return false;
-
-		auto decode_str = base64_decode(content);
-		file << decode_str;
-		file.close();
-		return true;
-	}
 };
 
 TEST_CASE(rpc_qps, true)
@@ -99,24 +33,6 @@ TEST_CASE(rpc_qps, true)
 	server s(9000, std::thread::hardware_concurrency()); //if you fill the last param, the server will remove timeout connections. default never timeout.
 	s.register_handler("add", &add);;
 	s.register_handler("translate", &messager::translate, &m);
-	s.register_handler("upload", &messager::upload, &m);
 
 	s.run();
-
-	//client
-	//boost::asio::io_service ios;
-	//client_proxy client(ios);
-	//client.connect("xxx.xxx...", "9000");
-	//client.call(...);
-	getchar();
-
-	std::uint64_t last_succeed_count = 0;
-
-	while (true)
-	{
-		auto curr_succeed_count = (std::uint64_t)g_succeed_count;
-		std::cout << curr_succeed_count - last_succeed_count << std::endl;
-		last_succeed_count = curr_succeed_count;
-		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-	}
 }
