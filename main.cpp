3489491572511e623297bd76ec5c614beed07db3
Gogs <gogs@fake.local>
2016-06-12 12:24:22 +0800

    move client project file to outer dir.

--- main.cpp
+++ main.cpp
@@ -28,5 +28,17 @@ int main()
 	s.run();
 
 	getchar();
+
+	//for test performance.
+	//std::uint64_t last_succeed_count = 0;
+
+	//while (true)
+	//{
+	//	auto curr_succeed_count = (std::uint64_t)g_succeed_count;
+	//	std::cout << curr_succeed_count - last_succeed_count << std::endl;
+	//	last_succeed_count = curr_succeed_count;
+	//	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+	//}
+
 	return 0;
 }
