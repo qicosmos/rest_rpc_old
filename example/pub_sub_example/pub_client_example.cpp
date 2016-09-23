1fd98853bc34eb9f34e61b19232032d40bea7b55
IndignantAngel <IndignentAngel@gmail.com>
2016-09-23 18:10:26 +0800

    test for pub and sub

--- example/pub_sub_example/pub_client_example.cpp
+++ example/pub_sub_example/pub_client_example.cpp
@@ -2,6 +2,35 @@
 
 namespace client
 {
+	struct configure
+	{
+		std::string hostname;
+		std::string port;
+
+		META(hostname, port);
+	};
+
+	configure get_config()
+	{
+		std::ifstream in("client.cfg");
+		std::stringstream ss;
+		ss << in.rdbuf();
+
+		configure cfg = { "127.0.0.1", "9000" };
+		DeSerializer dr;
+		try
+		{
+			dr.Parse(ss.str());
+			dr.Deserialize(cfg);
+		}
+		catch (const std::exception& e)
+		{
+			timax::SPD_LOG_ERROR(e.what());
+		}
+
+		return cfg;
+	}
+
 	TIMAX_DEFINE_PROTOCOL(sub_add, int(int, int));
 }
 
@@ -9,9 +38,13 @@ using async_client_t = timax::rpc::async_client<timax::rpc::msgpack_codec>;
 
 int main(void)
 {
-	timax::log::get().init("rest_rpc_client.lg");
+	timax::log::get().init("pub_client.lg");
 
-	auto endpoint = timax::rpc::get_tcp_endpoint("127.0.0.1", 9000);
+	auto config = client::get_config();
+
+	auto endpoint = timax::rpc::get_tcp_endpoint(config.hostname,
+		boost::lexical_cast<uint16_t>(config.port));
+
 	auto async_client = std::make_shared<async_client_t>();
 
 	try
